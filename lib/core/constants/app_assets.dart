d5e06180d930a4ec29de7d51b3ff0f48ad438c50
Muhammad Sufyan <108451771+Muhammad-Sufyan-901@users.noreply.github.com>
2026-03-05 09:09:15 +0700

    feat : adding static assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,18 @@
+class AppAssets {
+  // Main Assets Prefix
+  static const String assetsPrefix = 'assets';
+
+  // Images Prefix
+  static const String imagesPrefix = '$assetsPrefix/images';
+  static const String onboardingPrefix = '$imagesPrefix/onboarding';
+
+  // SVG Prefix
+  static const String svgPrefix = '$assetsPrefix/svg';
+  static const String svgIconPrefix = '$svgPrefix/icons';
+
+  // Onboarding Images
+  static const String onboardingPerson = '$onboardingPrefix/person.png';
+
+  // SVG Icons
+  static const String svgGoogle = '$svgIconPrefix/google.svg';
+}
